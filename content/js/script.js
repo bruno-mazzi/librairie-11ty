871212ec0efe24142fb522b19caabdaa92fc8395
Bruno Mazzi <houille@yahoo.fr>
2022-07-05 23:12:07 +0200

    add dark mode, refactor js and small fix css

--- content/js/script.js
+++ content/js/script.js
@@ -1,10 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
-
+function sortList() {
   document.querySelector('.filters').style.display = 'block';
     
-    var options = {
-        valueNames: [ 'ouvrage-title', { name: 'time', attr: 'data-time' } ]
-      };
+  var options = {
+      valueNames: [ 'ouvrage-title', { name: 'time', attr: 'data-time' } ]
+  };
       
-    var collectionList = new List('js-collection', options);
+  var collectionList = new List('js-collection', options);
+}
+
+function changeTheme() {
+  const btn = document.querySelector(".btn-toggle");
+    const currentTheme = localStorage.getItem("theme");
+
+    btn.classList.add("show-btn");
+
+
+    if (currentTheme == "dark") {
+        document.body.classList.toggle("dark-mode");
+        document.querySelector('.btn-toggle').innerHTML = 'Interface clair'
+    } else {
+        document.body.classList.toggle("light-mode");
+        document.querySelector('.btn-toggle').innerHTML = 'Interface sombre'
+    }
+
+
+    btn.addEventListener("click", function() {
+        if (document.body.classList.contains('light-mode')) {
+            document.body.classList.toggle("light-mode");
+            document.body.classList.toggle("dark-mode");
+            document.querySelector('.btn-toggle').innerHTML = 'Interface clair'
+            var theme = "dark";
+        } else {
+            document.body.classList.toggle("dark-mode");
+            document.body.classList.toggle("light-mode");
+            document.querySelector('.btn-toggle').innerHTML = 'Interface sombre'
+            var theme = "light";
+        }
+
+        localStorage.setItem("theme", theme);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  sortList()
+  changeTheme()
 });
